9f6eecf97ddd33190a1f990e808b8f617ba79e72
Daniel Seong <Daniel.Seong@cudirect.com>
2020-02-06 11:44:16 -0800

    made bsic mvc application to get used to routes and overall architecture of the framework

---
+++ MVCDemo/Controllers/PeopleController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            PersonModel p = new PersonModel();
+            int i = 5 / p.Age;
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+
+            people.Add(new PersonModel {FirstName = "Tim", LastName = "Corey", Age = 38});
+            people.Add(new PersonModel { FirstName = "Joe", LastName = "Smith", Age = 56 });
+            people.Add(new PersonModel { FirstName = "Sarah", LastName = "Corey", Age = 25 });
+
+            return View(people);
+        }
+    }
+}
--- MVCDemo/Models/PersonModel.cs
+++ MVCDemo/Models/PersonModel.cs
@@ -8,7 +8,7 @@ namespace MVCDemo.Models
     public class PersonModel
     {
         public string FirstName { get; set; }
-        public bool LastName { get; set; }
+        public string LastName { get; set; }
         public int Age { get; set; } = 0;
         public bool isAlive { get; set; } = true;
     }
