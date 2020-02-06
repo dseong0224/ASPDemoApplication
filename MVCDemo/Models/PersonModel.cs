0dfd1adb7681141e98301d2bff18cc3e82ac6d24
Daniel Seong <Daniel.Seong@cudirect.com>
2020-02-06 09:45:27 -0800

    first commit

---
+++ MVCDemo/Models/PersonModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public bool LastName { get; set; }
+        public int Age { get; set; } = 0;
+        public bool isAlive { get; set; } = true;
+    }
+}
